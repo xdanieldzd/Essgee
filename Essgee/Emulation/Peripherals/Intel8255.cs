1a42d157cce25d4db1fa1f95a83c9a038150dfb3
xdanieldzd <xdanieldzd@googlemail.com>
2019-08-08 20:20:32 +0200

    Revert i8255 to earlier implementation
    Fixes SG-1000 M2 Test Cartridge I/O test glitching; was not sure about the accuracy of the newer code to begin with

--- Essgee/Emulation/Peripherals/Intel8255.cs
+++ Essgee/Emulation/Peripherals/Intel8255.cs
@@ -11,15 +11,20 @@ namespace Essgee.Emulation.Peripherals
 		public byte PortAInput { get; set; }
 		public byte PortBInput { get; set; }
 		public byte PortCInput { get; set; }
-
 		public byte PortAOutput { get; set; }
 		public byte PortBOutput { get; set; }
 		public byte PortCOutput { get; set; }
 
-		/* Input/output mode */
-		int operatingModeGroupA;    /* Port A & upper port C */
-		int operatingModeGroupB;    /* Port B & lower port C */
-		bool isPortAInput, isPortCUInput, isPortBInput, isPortCLInput;
+		byte configByte, setResetControlByte;
+
+		int operatingModeGroupA => ((configByte >> 5) & 0x03);
+		bool isPortAInput => ((configByte & 0x10) == 0x10);
+		bool isPortCUInput => ((configByte & 0x08) == 0x08);
+		int operatingModeGroupB => ((configByte >> 2) & 0x01);
+		bool isPortBInput => ((configByte & 0x02) == 0x02);
+		bool isPortCLInput => ((configByte & 0x01) == 0x01);
+		int bitToChange => ((setResetControlByte >> 1) & 0x07);
+		bool isSetBitOperation => ((setResetControlByte & 0x01) == 0x01);
 
 		public Intel8255() { }
 
@@ -36,177 +41,40 @@ namespace Essgee.Emulation.Peripherals
 		{
 			switch (port & 0x03)
 			{
-				case 0x00:
-					/* Port A */
-					if (!isPortAInput)
-					{
-						switch (operatingModeGroupA)
-						{
-							case 0:
-								/* Simple I/O */
-								PortAOutput = value;
-								break;
-
-							default:
-								throw new Exception($"i8255: Unimplemented operating mode {operatingModeGroupA} writing Port A");
-						}
-					}
-					break;
-
-				case 0x01:
-					/* Port B */
-					if (!isPortBInput)
-					{
-						switch (operatingModeGroupB)
-						{
-							case 0:
-								/* Simple I/O */
-								PortBOutput = value;
-								break;
-
-							default:
-								throw new Exception($"i8255: Unimplemented operating mode {operatingModeGroupB} writing Port B");
-						}
-					}
-					break;
-
-				case 0x02:
-					/* Port C */
-					if (!isPortCUInput)
-					{
-						switch (operatingModeGroupA)
-						{
-							case 0:
-								/* Simple I/O */
-								PortCOutput &= 0x0F;
-								PortCOutput |= (byte)(value & 0xF0);
-								break;
-
-							default:
-								throw new Exception($"i8255: Unimplemented operating mode {operatingModeGroupA} writing Port C");
-						}
-					}
-					if (!isPortCLInput)
-					{
-						switch (operatingModeGroupB)
-						{
-							case 0:
-								/* Simple I/O */
-								PortCOutput &= 0xF0;
-								PortCOutput |= (byte)(value & 0x0F);
-								break;
-
-							default:
-								throw new Exception($"i8255: Unimplemented operating mode {operatingModeGroupB} writing Port C");
-						}
-					}
-					break;
+				case 0x00: PortAOutput = value; break;
+				case 0x01: PortBOutput = value; break;
+				case 0x02: PortCOutput = value; break;
 
 				case 0x03:
 					/* Control port */
 					if ((value & 0x80) == 0x80)
 					{
-						/* Input/output mode */
-						operatingModeGroupA = ((value >> 5) & 0x03);
-						isPortAInput = ((value & 0x10) == 0x10);
-						isPortCUInput = ((value & 0x08) == 0x08);
-						operatingModeGroupB = ((value >> 2) & 0x01);
-						isPortBInput = ((value & 0x02) == 0x02);
-						isPortCLInput = ((value & 0x01) == 0x01);
+						configByte = value;
 					}
 					else
 					{
-						/* Bit set/reset mode */
-						byte mask = (byte)(1 << ((value >> 1) & 0x07));
-						if ((value & 0x01) == 0x01)
-							PortCOutput |= mask;
-						else
-							PortCOutput &= (byte)~mask;
+						setResetControlByte = value;
+
+						byte mask = (byte)(1 << bitToChange);
+						if (isSetBitOperation) PortCOutput |= mask;
+						else PortCOutput &= (byte)~mask;
 					}
 					break;
 
-				default:
-					throw new Exception($"i8255: Unsupported write to port 0x{port:X2}, value 0x{value:X2}");
+				default: throw new Exception(string.Format("i8255: Unsupported write to port 0x{0:X2}, value 0x{1:X2}", port, value));
 			}
-
-			//System.IO.File.AppendAllText(@"D:\Temp\Essgee\ppi.txt", $"Port {port:X2}, value {value:X2}\n");
 		}
 
 		public byte ReadPort(byte port)
 		{
 			switch (port & 0x03)
 			{
-				case 0x00:
-					/* Port A */
-					if (isPortAInput)
-					{
-						switch (operatingModeGroupA)
-						{
-							case 0:
-								/* Simple I/O */
-								return PortAInput;
+				case 0x00: return (isPortAInput ? PortAInput : PortAOutput);
+				case 0x01: return (isPortBInput ? PortBInput : PortBOutput);
+				case 0x02: return (byte)(((isPortCUInput ? PortCInput : PortCOutput) & 0xF0) | (isPortCLInput ? PortCInput : PortCOutput) & 0x0F);
+				case 0x03: return 0xFF; /* Cannot read control port */
 
-							default:
-								throw new Exception($"i8255: Unimplemented operating mode {operatingModeGroupA} reading Port A");
-						}
-					}
-					// TODO: verify
-					return 0x00;
-
-				case 0x01:
-					/* Port B */
-					if (isPortBInput)
-					{
-						switch (operatingModeGroupB)
-						{
-							case 0:
-								/* Simple I/O */
-								return PortBInput;
-
-							default:
-								throw new Exception($"i8255: Unimplemented operating mode {operatingModeGroupB} reading Port B");
-						}
-					}
-					// TODO: verify
-					return 0x00;
-
-				case 0x02:
-					/* Port C */
-					var value = (byte)0x00; // TODO: verify
-					if (isPortCUInput)
-					{
-						switch (operatingModeGroupA)
-						{
-							case 0:
-								/* Simple I/O */
-								value |= (byte)(PortCInput & 0xF0);
-								break;
-
-							default:
-								throw new Exception($"i8255: Unimplemented operating mode {operatingModeGroupA} reading Port C[U]");
-						}
-					}
-					if (isPortCLInput)
-					{
-						switch (operatingModeGroupB)
-						{
-							case 0:
-								/* Simple I/O */
-								value |= (byte)(PortCInput & 0x0F);
-								break;
-
-							default:
-								throw new Exception($"i8255: Unimplemented operating mode {operatingModeGroupB} reading Port C[L]");
-						}
-					}
-					return value;
-
-				case 0x03:
-					/* Cannot read control port */
-					return 0xFF;
-
-				default:
-					throw new Exception($"i8255: Unsupported read from port 0x{port:X2}");
+				default: throw new Exception(string.Format("i8255: Unsupported read from port 0x{0:X2}", port));
 			}
 		}
 	}
